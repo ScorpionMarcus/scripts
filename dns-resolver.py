d1a6c226cda3c40377ac17779b9e8379b3921069
Marcus LeGault <marcus.legault@scorpion.co>
2019-01-01 20:14:04 -0800

    added try/except for records that don't exist; refactored the query process into a function

--- dns-resolver.py
+++ dns-resolver.py
@@ -1,36 +1,51 @@
 import dns.resolver
 import os
 import whois
+import random
 
 '''
 TODO
+before doing anything else, check to see if the site exists
 '''
 
 print('Enter domain: ')
 url = input().replace('https', '').replace('http', '').replace('/', '').replace(':', '').replace('www.', '').strip()
 myResolver = dns.resolver.Resolver()
 user = os.environ['USERPROFILE']
+arr = []
 
-a = myResolver.query(url, 'A')
-ns = myResolver.query(url, 'NS')
-mx = myResolver.query(url, 'MX')
-txt = myResolver.query(url, 'TXT')
-cname = myResolver.query('www.' + url, 'CNAME')
-whois = whois.whois(url).text
-arr = a, cname, ns, mx, txt
+print('\nQuerying ' + url + '...')
+
+def ping(record_type):
+    try:
+        if record_type == 'CNAME':
+            record = myResolver.query('www.' + url, record_type)
+        else:
+            record = myResolver.query(url, record_type)
+        arr.append(record)
+    except:
+        print('No ' + record_type + ' records found')
+
+for i in ['A', 'CNAME', 'NS', 'MX', 'TXT']:
+    ping(i)
 
 try:
     save_path = user + '/Desktop/' + url
     os.makedirs(save_path)
-
 except FileExistsError:
     save_path = user + '/Desktop/' + url + str(random.randint(1,101))
     os.makedirs(save_path)
+
 readme = open(save_path + '/' + 'README.txt', 'a')
 
 for rdata in arr:
     for i in rdata:
         readme.write(str(i) + '\n\n')
-readme.write(whois)
+
+try:
+    whois = whois.whois(url).text
+    readme.write(whois)
+except:
+    print('Unable to retrieve WHOIS')
 
 print('README saved to ' + save_path)

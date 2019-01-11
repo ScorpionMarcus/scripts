a37d4c3ca17f3cc90d22c67195b904e41df510ac
Marcus LeGault <marcus.legault@scorpion.co>
2019-01-11 09:36:11 -0800

    retake.py changed to open incognito chrome

--- retake.py
+++ retake.py
@@ -22,10 +22,8 @@ url = [
 for i in url:
 	try:
 		print(i + " -- retaking screenshot")
-		chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+		chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s --incognito'
 		webbrowser.get(chrome_path).open_new_tab('https://sms.scorpiondesign.com/cms/screenshots/' + 'www.' + i + '.png?retake=true')
-		print(i + " -- screenshot retaken successfully\n")
-
 	except:
 		print(i + " -- RETAKE ERROR")
 

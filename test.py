4ada6d9d1edb6fc5b2be4b7b5dab3b8070e2b0a3
Marcus LeGault <marcus.legault@scorpion.co>
2019-04-23 19:35:22 -0700

    removed test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-request = requests.get('http://www.tesdfsdfsdfdfxcsd.com')
-
-if request.status_code == 200:
-    print('Web site exists')
-else:
-    print('Web site does not exist') 
